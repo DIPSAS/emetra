31e6299d5077817cea9da9a4553bba57f428ffec
Magne Rekdal <mrk@dips.no>
2020-04-05 18:31:37 +0200

    Added base class for business objects, includes verification of constructor parameters, logging, and call stack support for logging.

---
+++ Business/Emetra.Business.BaseClass.pas
@@ -0,0 +1,90 @@
+﻿unit Emetra.Business.BaseClass;
+
+interface
+
+uses
+  {General interfaces}
+  Emetra.Logging.Interfaces,
+  Emetra.Dictionary.Interfaces,
+  {Standard}
+  System.Classes, System.Generics.Collections;
+
+type
+  /// <summary>
+  /// Business objects are lightweight objects that can be used as base class
+  /// for more complex objects that contains business logic. Typically they
+  /// are not used for smaller data objects with limited scope. These may not
+  /// need a logger, and they don't need to implement interfaces. More
+  /// complex data objects like <b>TPatient</b> in en EHR application may
+  /// need both of these. Deriving from <b>TInterfacedPersistent</b> (a
+  /// standard RTL class) it is not garbage collected and must be freed when
+  /// it is no longer used. If it it is added to a <b>TBusinessObjectCatalog</b>
+  /// , the lifetime and destruction is handled by the catalog.
+  /// </summary>
+  /// <remarks>
+  /// All TCustomBusiness descendants need to have a logger injected upon
+  /// construction.
+  /// </remarks>
+  /// <seealso cref="TBusinessObjectCatalog" />
+  TCustomBusiness = class( TInterfacedPersistent )
+  strict private
+    fLog: ILog;
+  protected
+    procedure CheckAssigned( AInterface: IInterface; const ANameOfInterface: string );
+    procedure EnterMethod( const AProcName: string );
+    procedure LeaveMethod( const AProcName: string );
+    procedure VerifyConstructorParameters; dynamic;
+  public
+    constructor Create( ALog: ILog ); reintroduce;
+    property Log: ILog read fLog;
+  end;
+
+implementation
+
+uses
+  System.SysUtils;
+
+constructor TCustomBusiness.Create( ALog: ILog );
+begin
+  inherited Create;
+  fLog := ALog;
+end;
+
+procedure TCustomBusiness.EnterMethod( const AProcName: string );
+begin
+  fLog.EnterMethod( Self, AProcName );
+end;
+
+procedure TCustomBusiness.LeaveMethod( const AProcName: string );
+begin
+  fLog.LeaveMethod( Self, AProcName );
+end;
+
+procedure TCustomBusiness.VerifyConstructorParameters;
+begin
+  CheckAssigned( fLog, 'Log' );
+end;
+
+procedure TCustomBusiness.CheckAssigned( AInterface: IInterface; const ANameOfInterface: string );
+const
+  PROC_NAME = 'CheckAssigned';
+  ERR_MSG   = LOG_STUB_STRING + 'The interface is not assigned.';
+var
+  errorMessage: string;
+begin
+  if not Assigned( AInterface ) then
+  begin
+    errorMessage := Format( ERR_MSG, [ClassName, PROC_NAME, ANameOfInterface] );
+    if Assigned( fLog ) then
+      fLog.SilentError( errorMessage )
+    else if Assigned( GlobalLog ) then
+      GlobalLog.SilentError( errorMessage );
+    raise EArgumentNilException.Create( errorMessage );
+  end;
+end;
+
+{$ENDREGION}
+
+begin
+
+end.
