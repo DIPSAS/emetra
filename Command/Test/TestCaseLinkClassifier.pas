1b4eceb03d27dba92940b673666ed91cc3ece574
Magne Rekdal <mrk@dips.no>
2020-04-13 00:40:47 +0200

    Added more tests, fixed bug with empty URL, fixed testcases (using comma separated parameters).

--- Command/Test/TestCaseLinkClassifier.pas
+++ Command/Test/TestCaseLinkClassifier.pas
@@ -5,7 +5,8 @@ interface
 uses
   Emetra.Logging.Interfaces,
   Emetra.Command.LinkClassifier,
-  DUnitX.TestFramework;
+  DUnitX.TestFramework,
+  System.Classes;
 
 type
 
@@ -13,6 +14,7 @@ type
   TTestClassLinkParser = class( TObject )
   private
     fLinkParser: TLinkParser;
+    fParams: TStringList;
   public
     [Setup]
     procedure Setup;
@@ -29,10 +31,11 @@ type
     [TestCase( 'http shortest', 'http://kvalitet' )]
     procedure TestIsLink( const AUrl: string );
     [Test]
-    [TestCase( 'AddForm', '//AddForm?FormName="GBD_INNLEGGELSE"', 'FormName' ) ]
-    [TestCase( 'ActiveCase', '//ActiveCase?StatusId=25', 'StatusId' ) ]
-    [TestCase( 'AlertSnooze', '//AlertSnooze?AlertId=n&Delay=14', 'AlertId' ) ]
-    procedure TestIsApplicationCommand( const AUrl: string; const AFirstParam: string );
+    [TestCase( 'about with AddForm', 'about://AddForm?FormName=TESTFORM,AddForm,FormName' )]
+    [TestCase( 'Snooze', '//AlertSnooze?AlertId=12,AlertSnooze,AlertId' )]
+    procedure TestIsApplicationCommand( const AUrl: string; const ACommandName, AFirstParam: string );
+    [Test]
+    procedure TestManyApplicationCommands;
   end;
 
 implementation
@@ -40,23 +43,39 @@ implementation
 procedure TTestClassLinkParser.Setup;
 begin
   fLinkParser := TLinkParser.Create;
+  fParams := TStringList.Create;
 end;
 
 procedure TTestClassLinkParser.Teardown;
 begin
+  fParams.Free;
   fLinkParser.Free;
 end;
 
 procedure TTestClassLinkParser.TestIsLink( const AUrl: string );
 begin
-  Assert.AreEqual( ord( fLinkParser.ClassifyUrl( AUrl ) ), ord( urlPassThrough ), 'This link should be classified as pass through (let the browser handle it)' );
+  Assert.AreEqual( ord( urlPassThrough ), ord( fLinkParser.ClassifyUrl( AUrl ) ), 'This link should be classified as pass through (let the browser handle it)' );
 end;
 
-procedure TTestClassLinkParser.TestIsApplicationCommand(const AUrl: string; const AFirstParam: string);
+procedure TTestClassLinkParser.TestManyApplicationCommands;
 begin
-  Assert.AreEqual( ord( fLinkParser.ClassifyUrl( AUrl ) ), ord( urlApplicationCommand ), 'This link should be classified as application command' );
+  TestIsApplicationCommand( 'about://AddForm?FormName="GBD_INNLEGGELSE"', 'AddForm', 'FormName' );
+  TestIsApplicationCommand( 'about://ActiveCase?StatusId=25', 'ActiveCase', 'StatusId' );
+  TestIsApplicationCommand( 'about://AlertSnooze?AlertId=n&Delay=14', 'AlertSnooze', 'AlertId' );
+  TestIsApplicationCommand( '//AddForm?FormName=GBD_INNLEGGELSE', 'AddForm', 'FormName' );
+  TestIsApplicationCommand( '//ActiveCase?StatusId=25', 'ActiveCase', 'StatusId' );
+  TestIsApplicationCommand( '//AlertSnooze?AlertId=n&Delay=14', 'AlertSnooze', 'AlertId' );
 end;
 
+procedure TTestClassLinkParser.TestIsApplicationCommand( const AUrl: string; const ACommandName, AFirstParam: string );
+var
+  commandName: string;
+begin
+  Assert.AreEqual( ord( urlApplicationCommand ), ord( fLinkParser.ClassifyUrl( AUrl ) ), 'This link should be classified as application command' );
+  Assert.IsTrue( fLinkParser.TryParseCommand( AUrl, commandName, fParams ) );
+  Assert.AreEqual( ACommandName, commandName, 'Unexpected command name' );
+  Assert.AreEqual( AFirstParam, fParams.Names[0], 'Unexcpected parameter name' );
+end;
 
 initialization
 
