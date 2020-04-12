9d5bfbbc1a4fe91b97c1ab5c87a533a6e476a14e
Magne Rekdal <mrk@dips.no>
2020-04-13 00:40:47 +0200

    Added more tests, fixed bug with empty URL, fixed testcases (using comma separated parameters).

--- Command/Emetra.Command.LinkClassifier.pas
+++ Command/Emetra.Command.LinkClassifier.pas
@@ -13,6 +13,7 @@ type
   private
     fTokenizer: TTokenizer;
     fOldStyleCommandFinder: TRegEx;
+    fNewStyleCommandFinder: TRegEx;
   public
     procedure AfterConstruction; override;
     procedure BeforeDestruction; override;
@@ -29,7 +30,9 @@ uses
 procedure TLinkParser.AfterConstruction;
 begin
   inherited;
+  { TRegEx is a record, no need to free before destruction }
   fOldStyleCommandFinder := TRegEx.Create( '(AddForm|AddDx|Print)=', [roIgnoreCase] );
+  fNewStyleCommandFinder := TRegEx.Create( '^(about:)?//' );
   fTokenizer := TTokenizer.Create;
 end;
 
@@ -50,36 +53,42 @@ function TLinkParser.ClassifyUrl( const AURL: string; out AFileName: string ): T
 var
   fileExt: string;
 begin
-  { A link to html-file is prefixed with report:// and has a '/' at the end }
-  AFileName := StringReplace( AURL, 'report://', '', [] ).TrimRight( ['/'] );
-  fileExt := ExtractFileExt( AFileName );
   Result := urlPassThrough;
-  if Pos( 'TfrmDocViewer', AFileName ) > 1 then
-    Result := urlReportOutput
-  else if ( AURL[1] = '/' ) or fOldStyleCommandFinder.IsMatch( AURL ) then
-    Result := urlApplicationCommand
-  else if FileExists( AFileName ) then
+  if Length( AURL ) > 0 then
   begin
-    if SameText( fileExt, '.html' ) then
-      Result := urlReportTemplateFile
-    else if SameText( fileExt, '.fr3' ) then
-      Result := urlFastReport
-    else if SameText( fileExt, '.pdf' ) then
-      Result := urlPDF
-    else
-      Result := urlOther;
+    { A link to html-file is prefixed with report:// and has a '/' at the end }
+    AFileName := StringReplace( AURL, 'report://', '', [] ).TrimRight( ['/'] );
+    fileExt := ExtractFileExt( AFileName );
+    if Pos( 'TfrmDocViewer', AFileName ) > 1 then
+      Result := urlReportOutput
+    else if fNewStyleCommandFinder.IsMatch( AURL ) or fOldStyleCommandFinder.IsMatch( AURL ) then
+      Result := urlApplicationCommand
+    else if FileExists( AFileName ) then
+    begin
+      if SameText( fileExt, '.html' ) then
+        Result := urlReportTemplateFile
+      else if SameText( fileExt, '.fr3' ) then
+        Result := urlFastReport
+      else if SameText( fileExt, '.pdf' ) then
+        Result := urlPDF
+      else
+        Result := urlOther;
+    end;
   end;
 end;
 
 function TLinkParser.TryParseCommand( const AURL: string; out ACommandName: string; AParams: TStrings ): boolean;
 var
-  match: TMatch;
+  newMatch: TMatch;
+  oldMatch: TMatch;
 begin
   Result := false;
-  if Pos( '//', AURL ) = 1 then
+  newMatch := fNewStyleCommandFinder.match( AURL );
+  oldMatch := fOldStyleCommandFinder.match( AURL );
+  if newMatch.Success then
   begin
     { New style command, like URL: "about://AlertSnooze?AlertId=n&Delay=14" }
-    ACommandName := Copy( AURL, 3, maxint );
+    ACommandName := Copy( AURL, Pos( '//', AURL ) + 2, maxint );
     fTokenizer.Prepare( ACommandName, '?' );
     ACommandName := fTokenizer[0];
     AParams.Delimiter := '&';
@@ -87,26 +96,22 @@ begin
     AParams.DelimitedText := fTokenizer[1];
     Result := true;
   end
-  else
+  else if oldMatch.Success then
   begin
-    { Old style command handling }
-    match := fOldStyleCommandFinder.match( AURL );
-    if match.Success then
+    { Old style command, single key/value like this: "AddForm=MADRS" }
+    fTokenizer.Prepare( Copy( AURL, oldMatch.Index, maxint ), '=' );
+    ACommandName := fTokenizer[0];
+    if SameText( ACommandName, 'AddForm' ) then
+      AParams.Values['FormName'] := fTokenizer[1]
+    else if SameText( ACommandName, 'Print' ) then
+      AParams.Values['FileName'] := fTokenizer[1]
+    else if SameText( ACommandName, 'AddDx' ) then
     begin
-      fTokenizer.Prepare( Copy( AURL, match.Index, maxint ), '=' );
-      ACommandName := fTokenizer[0];
-      if SameText( ACommandName, 'AddForm' ) then
-        AParams.Values['FormName'] := fTokenizer[1]
-      else if SameText( ACommandName, 'Print' ) then
-        AParams.Values['FileName'] := fTokenizer[1]
-      else if SameText( ACommandName, 'AddDx' ) then
-      begin
-        fTokenizer.Prepare( fTokenizer[1], ':' );
-        AParams.Values['ListId'] := fTokenizer[0];
-        AParams.Values['DxCode'] := fTokenizer[1];
-      end;
-      Result := true;
+      fTokenizer.Prepare( fTokenizer[1], ':' );
+      AParams.Values['ListId'] := fTokenizer[0];
+      AParams.Values['DxCode'] := fTokenizer[1];
     end;
+    Result := true;
   end;
 end;
 
