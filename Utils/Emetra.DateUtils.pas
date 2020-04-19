a4e32902ca858770b81dbc1fa3869f251eb5d744
Magne Rekdal <mrk@dips.no>
2020-04-19 12:46:27 +0200

    Added some more hashing functions to utility folders. Added unit for date utilities.

---
+++ Utils/Emetra.DateUtils.pas
@@ -0,0 +1,48 @@
+﻿unit Emetra.DateUtils;
+
+interface
+
+uses
+  System.DateUtils;
+
+function TryISO8601ToDate( const AISODate: string; out Value: TDateTime; AReturnUTC: Boolean = True ): Boolean;
+function YearsOld( const ANow, ADateOfBirth: TDateTime ): integer;
+function BirthdayToday( const ADateOfBirth: TDateTime ): Boolean;
+
+implementation
+
+uses
+  System.SysUtils;
+
+function IsNeg( const AExpression: double ): integer;
+begin
+  if AExpression < 0 then
+    Result := 1
+  else
+    Result := 0;
+end;
+
+function YearsOld( const ANow, ADateOfBirth: TDateTime ): integer;
+begin
+  Result := YearOf( ANow ) - YearOf( ADateOfBirth ) - IsNeg( ( MonthOf( ANow ) - MonthOf( ADateOfBirth ) ) * 50 + DayOf( ANow ) - DayOf( ADateOfBirth ) );
+end;
+
+function BirthdayToday( const ADateOfBirth: TDateTime ): Boolean;
+begin
+  Result := ( MonthOf( Now ) = MonthOf( ADateOfBirth ) ) and ( DayOf( Now ) = DayOf( ADateOfBirth ) );
+end;
+
+function TryISO8601ToDate( const AISODate: string; out Value: TDateTime; AReturnUTC: Boolean = True ): Boolean;
+begin
+  Result := False;
+  if AISODate <> '' then
+    try
+      Value := ISO8601ToDate( AISODate, AReturnUTC );
+      Result := True
+    except
+      on Exception do
+        { Swallow this exception for very good reasons }
+    end;
+end;
+
+end.
---
+++ Utils/Emetra.Hash.Mod10.pas
@@ -0,0 +1,40 @@
+﻿unit Emetra.Hash.Mod10;
+
+interface
+
+function Mod10(const Value: string): Integer;
+
+implementation
+
+Uses SysUtils;
+
+function Mod10(const Value: string): Integer;
+var
+  i, intOdd, intEven: Integer;
+begin
+  {add all odd seq numbers}
+  intOdd := 0;
+  i := 1;
+  while (i <= Length(Value)) do
+  begin
+    Inc(intOdd, StrToIntDef(Value[i], 0));
+    Inc(i, 2);
+  end;
+
+  {add all even seq numbers}
+  intEven := 0;
+  i := 2;
+  while (i <= Length(Value)) do
+  begin
+    Inc(intEven, StrToIntDef(Value[i], 0));
+    Inc(i, 2);
+  end;
+
+  Result := 3*intOdd + intEven;
+  {modulus by 10 to get}
+  Result := Result mod 10;
+  if Result <> 0 then
+    Result := 10 - Result
+end;
+
+end.
