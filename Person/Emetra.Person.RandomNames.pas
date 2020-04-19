af56228be01e766c39397a216073758036c00034
Magne Rekdal <mrk@dips.no>
2020-04-19 12:05:53 +0200

    Added simple class for IPersonReadOnly and IPersonIdentity used for testing. Added class for generating random names, predomniantly in Norwegian but names from Germany, Usa and Hungary are also includes, and the most common name in every European country.

---
+++ Person/Emetra.Person.RandomNames.pas
@@ -0,0 +1,263 @@
+﻿unit Emetra.Person.RandomNames;
+
+interface
+
+uses
+  System.Classes;
+
+const
+  { ClassPatient }
+  sexUnknown = 0;
+  sexMale    = 1;
+  sexFemale  = 2;
+
+type
+  TNameList = class( TStringList )
+    procedure AddMultiple( const ANames: string );
+    procedure AfterConstruction; override;
+  end;
+
+  TRandomNames = class( TObject )
+  strict private
+    fGirls: TNameList;
+    fBoys: TNameList;
+    fSurnames: TNameList;
+    fDoubleSurnames: boolean;
+    fDoubleFirstnames: boolean;
+  public
+    procedure AfterConstruction; override;
+    procedure BeforeDestruction; override;
+    function RandomSex: integer;
+    function RandomName( const ASex: integer ): string;
+    function RandomFirstname( const ASex: integer ): string;
+    function RandomSurname: string;
+    function RandomDoctor: string;
+    { Properties }
+    property DoubleFirstnames: boolean read fDoubleFirstnames write fDoubleFirstnames;
+    property DoubleSurnames: boolean read fDoubleSurnames write fDoubleSurnames;
+  end;
+
+implementation
+
+uses
+  System.SysUtils;
+
+{ TNameList }
+
+procedure TNameList.AfterConstruction;
+begin
+  inherited;
+  Duplicates := dupIgnore;
+end;
+
+procedure TNameList.AddMultiple( const ANames: string );
+var
+  lstNames: TNameList;
+begin
+  lstNames := TNameList.Create;
+  try
+    lstNames.CommaText := ANames;
+    while lstNames.Count > 0 do
+    begin
+      Self.Add( lstNames[0] );
+      lstNames.Delete( 0 );
+    end;
+  finally
+    lstNames.Free;
+  end;
+end;
+
+{ TRandomNames }
+
+procedure TRandomNames.AfterConstruction;
+begin
+  inherited;
+  fSurnames := TNameList.Create;
+  fBoys := TNameList.Create;
+  fGirls := TNameList.Create;
+  FUndefined := TNameList.Create;
+
+  with FUndefined do
+  begin
+    AddMultiple( 'Alex,Bobby,Chris,Jone,Nicola' );
+  end;
+  
+  with fBoys do
+  begin
+    { Guttenavn 50 på topp 2020 }
+    AddMultiple( 'Jan,Per,Bjørn,Ole,Lars,Kjell,Knut,Svein,Thomas,Arne' ); // 10
+    AddMultiple( 'Geir,Hans,Morten,Tor,Martin,Terje,Erik,Andreas,Odd,Anders' ); // 20
+    AddMultiple( 'John,Rune,Daniel,Trond,Tore,Kristian,Marius,Jon,Magnus,Stian' ); // 30
+    AddMultiple( 'Tom,Olav,Henrik,Harald,Espen,Fredrik,Øyvind,Jonas,Christian,Eirik' ); // 40
+    AddMultiple( 'Gunnar,Rolf,Leif,Nils,Håkon,Helge,Einar,Steinar,Frode,Øystein' ); // 50
+    { Tyske 10 på topp 1890-2006 }
+    AddMultiple( 'Peter,Mikael,Thomas,Andreas,Wolfgang,Klaus,Jürgen,Günther,Stefan,Kristian' );
+    { Ungarske 10 på topp }
+    AddMultiple( 'László,István,József,János,Zoltán,Sándor,Ferenc,Gábor,Attila,Péter' );
+    { USA 20 på topp 2018 }
+    AddMultiple( 'Liam,Noah,William,James,Oliver,Benjamin,Elijah,Lucas,Mason,Logan,Alexander,Ethan,Jacob,Michal,Daniel,Henry,Jackson,Sebastian,Aiden,Matthew' );
+  end;
+
+  with fGirls do
+  begin
+    { Jentenavn 50 på topp 2020 }
+    AddMultiple( 'Anne,Inger,Kari,Marit,Ingrid,Liv,Eva,Anna,Maria,Ida' ); // 10
+    AddMultiple( 'Hilde,Berit,Nina,Marianne,Astrid,Elisabeth,Bjørg,Kristin,Randi,Solveig' ); // 20
+    AddMultiple( 'Bente,Heidi,Silje,Hanne,Linda,Tone,Anita,Elin,Tove,Wenche' ); // 30
+    AddMultiple( 'Gerd,Camilla,Ragnhild,Ellen,Hege,Karin,Ann,Julie,Mona,Marie' ); // 40
+    AddMultiple( 'Monica,Aud,Else,Kristine,Laila,Turid,Stine,Helene,Mari,Emma' ); // 50
+    { Tyske 10 på topp 1890-2006 }
+    AddMultiple( 'Ursula,Karin,Helga,Sabine,Ingrid,Renate,Monika,Susanne,Gisela,Petra' );
+    { Ungarske 10 på topp }
+    AddMultiple( 'Mária,Erzsébet,Ilona,Katalin,Éva,Anna,Margit,Zsuzsanna,Julianna,Judit' );
+    { USA 20 på topp 2018 }
+    AddMultiple( 'Emma,Olivia,Ava,Isabella,Sophia,Charlotte,Mia,Amelia,Harper,Evelyn,Abigail,Emily,Elizabeth,Mila,Ella,Avery,Sofia,Camila,Aria,Scarlett' )
+  end;
+
+  with fSurnames do
+  begin
+    { 200 vanligste etternavn i Norge 2001 }
+    AddMultiple( 'Hansen,Olsen,Johansen,Larsen,Andersen,Nilsen,Pedersen,Kristiansen,Jensen,Karlsen' );
+    AddMultiple( 'Johnsen,Pettersen,Eriksen,Berg,Haugen,Hagen,Johannessen,Andreassen,Jacobsen,Dahl' );
+    AddMultiple( 'Jørgensen,Henriksen,Halvorsen,Lund,Sørensen,Jakobsen,Moen,Gundersen,Iversen,Strand' );
+    AddMultiple( 'Svendsen,Solberg,Martinsen,Eide,Knutsen,Paulsen,Bakken,Kristoffersen,Mathisen,Lie' );
+    AddMultiple( 'Amundsen,Rasmussen,Nguyen,Lunde,Solheim,Moe,Berge,Nygård,Kristensen,Bakke' );
+    AddMultiple( 'Ali,Fredriksen,Holm,Lien,Hauge,Andresen,Christensen,Nielsen,Knudsen,Evensen' );
+    AddMultiple( 'Sæther,Aas,Myhre,Hanssen,Haugland,Thomassen,Sivertsen,Simonsen,Ahmed,Danielsen' );
+    AddMultiple( 'Berntsen,Rønning,Sandvik,Arnesen,Næss,Antonsen,Vik,Haug,Ellingsen,Thorsen' );
+    AddMultiple( 'Edvarsen,Birkeland,Isaksen,Gulbrandsen,Ruud,Aasen,Strøm,Myklebust,Tangen,Ødegård' );
+    AddMultiple( 'Eliassen,Helland,Bøe,Jenssen,Aune,Mikkelsen,Tveit,Brekke,Abrahamsen,Madsen' );
+    AddMultiple( 'Engen,Christiansen,Sunde,Bjerke,Mortensen,Torgersen,Thoresen,Hermansen,Mikalsen,Magnussen' );
+    AddMultiple( 'Gjerde,Helgesen,Hovland,Nilssen,Wold,Bråthen,Dale,Mohamed,Nygaard,Dahle' );
+    AddMultiple( 'Eilertsen,Wilhelmsen,Steen,Foss,Hassan,Bjørnstad,Håland,Jansen,Sætre,Gabrielsen' );
+    AddMultiple( 'Hammer,Tran,Gustavsen,Ingebrigtsen,Hoel,Bråten,Solli,Holmen,Lorentzen,Hoff,Monsen' );
+    AddMultiple( 'Breivik,Solbakken,Solvang,Fjeld,Egeland,Nordby,Sand,Aase,Jonassen,Bø' );
+    AddMultiple( 'Stokke,Dalen,Bye,Johannesen,Johanson,Wiik,Andersson,Syversen,Wang,Ødegaard' );
+    AddMultiple( 'Løken,Sørlie,Haga,Møller,Sandberg,Tollefsen,Ibrahim,Sande,Haaland,Viken' );
+    AddMultiple( 'Teigen,Berger,Hamre,Eikeland,Kvam,Enger,Torp,Kolstad,Ottesen,Borge' );
+    AddMultiple( 'Øien,Holen,Fosse,Langeland,Stene,Nikolaisen,Helle,Kvamme,Skoglund,Sletten' );
+
+    { Vanligste etternavn i Europa }
+    Add( 'Almeida' ); // Portugal
+    Add( 'Andersson' ); // Sverige
+    Add( 'Angelovski' ); // Makedonia
+    Add( 'Bērziņš' ); // Latvia
+    Add( 'Beqiri' ); // Albania
+    Add( 'De Jong' ); // Nederland
+    Add( 'Dimitrov' ); // Bulgaria
+    Add( 'García' ); // Spania
+    Add( 'Gruber' ); // Østerrike
+    Add( 'Hodžić' ); // Bosnia
+    Add( 'Horvat' ); // Kroatia
+    Add( 'Hoxha' ); // Kosovo
+    Add( 'Ivanov' ); // Belarus
+    Add( 'Joensen' ); // Færøyene
+    Add( 'Jones' ); // Wales
+    Add( 'Jovanović' ); // Serbia
+    Add( 'Kazlauskas' ); // Litauen
+    Add( 'Korhonen' ); // Finland
+    Add( 'Martin' ); // Frankrike
+    Add( 'Melnik' ); // Ukraina
+    Add( 'Murphy' ); // Irland
+    Add( 'Müller' ); // Tyskland og Sveits
+    Add( 'Nagy' ); // Ungarn
+    Add( 'Novak' ); // Slovenia
+    Add( 'Novák' ); // Tsjekkia
+    Add( 'Nowak' ); // Polen
+    Add( 'Peeters' ); // Belgia
+    Add( 'Popescu' ); // Romania
+    Add( 'Popović' ); // Montenegro
+    Add( 'Rossi' ); // Italia
+    Add( 'Smirnov' ); // Russland
+    Add( 'Smith' ); // England og Skottland
+    Add( 'Tamm' ); // Estland
+    Add( 'Wilson' ); // Nord-Irland
+  end;
+end;
+
+procedure TRandomNames.BeforeDestruction;
+begin
+  fUndefined.Free;
+  fSurnames.Free;
+  fGirls.Free;
+  fBoys.Free;
+  inherited;
+end;
+
+function TRandomNames.RandomSex: integer;
+begin
+  Result := Random( 20 ) mod 2 + 1;
+end;
+
+function TRandomNames.RandomFirstname( const ASex: integer ): string;
+var
+  firstname1: string;
+  firstname2: string;
+begin
+  Result := EmptyStr;
+  case ASex of
+    sexUnknown:
+      begin
+        Result := FUndefined[Random(fUndefined.Count )];;
+        exit;
+      end;
+    sexFemale:
+      begin
+        firstname1 := fGirls[Random( fGirls.Count )];
+        firstname2 := fGirls[Random( fGirls.Count )];
+      end;
+    sexMale:
+      begin
+        firstname1 := fBoys[Random( fBoys.Count )];
+        firstname2 := fBoys[Random( fBoys.Count )];
+      end;
+  end;
+  if not fDoubleFirstnames then
+    Result := firstname1
+  else
+  begin
+    if firstname1 = firstname2 then
+      Result := firstname1
+    else if Random( 2 ) = 0 then
+      Result := firstname2
+    else
+    begin
+      Result := firstname1 + ' ' + firstname2;
+      if Random( 5 ) = 0 then
+        Result := StringReplace( Result, ' ', '-', [] );
+    end;
+  end;
+end;
+
+function TRandomNames.RandomSurname: string;
+var
+  surname1: string;
+  surname2: string;
+begin
+  { Prepare name }
+  surname1 := fSurnames[Random( fSurnames.Count )];
+  if not fDoubleSurnames then
+    Result := surname1
+  else
+  begin
+    surname2 := fSurnames[Random( fSurnames.Count )];
+    if surname1 = surname2 then
+      Result := surname1
+    else if Random( 6 ) = 0 then
+      Result := surname1 + ' ' + surname2;
+    if Random( 20 ) = 0 then
+      Result := StringReplace( Result, ' ', '-', [] );
+  end;
+end;
+
+function TRandomNames.RandomName( const ASex: integer ): string;
+begin
+  Result := RandomSurname + ', ' + RandomFirstname( ASex );
+end;
+
+function TRandomNames.RandomDoctor: string;
+begin
+  Result := Format( 'DR%d', [Random( 8 ) + 1] );
+end;
+
+end.
---
+++ Person/Emetra.Person.Simple.pas
@@ -0,0 +1,172 @@
+﻿unit Emetra.Person.Simple;
+
+interface
+
+uses
+  Emetra.Person.Interfaces,
+  Emetra.Database.Interfaces,
+  {Standard}
+  System.Classes, System.SysUtils;
+
+type
+  EPersonNotFoundException = class( Exception );
+
+  TSimplePerson = class( TInterfacedPersistent, IPersonReadOnly, IPersonIdentity )
+  private
+    fPersonId: integer;
+    fDOB: TDate;
+    fSex: TSex;
+    fLstName: string;
+    fFstName: string;
+    fNationalId: string;
+    procedure Set_GenderId( const Value: integer );
+    procedure Set_PersonId( const Value: integer );
+  protected
+    { Property accessors }
+    function Get_Age: double;
+    function Get_DOB: TDate;
+    function Get_FirstName: string;
+    function Get_FullName: string;
+    function Get_GenderId: integer;
+    function Get_LastName: string;
+    function Get_NationalId: string;
+    function Get_PersonId: integer;
+    function Get_Sex: TSex;
+    function Get_VisualId: string;
+    { Other members }
+    function ShortId: string;
+    function Valid: boolean;
+  public
+    procedure Clear;
+    procedure MapId( const ASQL: ISQL );
+    { Prioerties }
+    property Age: double read Get_Age;
+    property DOB: TDate read Get_DOB write fDOB;
+    property FirstName: string read Get_FirstName write fFstName;
+    property FullName: string read Get_FullName;
+    property GenderId: integer read Get_GenderId write Set_GenderId;
+    property LastName: string read Get_LastName write fLstName;
+    property NationalId: string read Get_NationalId write fNationalId;
+    property PersonId: integer read Get_PersonId write Set_PersonId;
+    property Sex: TSex read Get_Sex write fSex;
+    property VisualId: string read Get_VisualId;
+  end;
+
+implementation
+
+uses
+  System.DateUtils;
+
+resourcestring
+  StrPersonNotFound = 'Fant ingen person med fødselsnummer "%s" i denne databasen.';
+
+const
+  QRY_PERSON = 'SELECT PersonId, DOB, GenderId, FstName, LstName FROM dbo.Person WHERE NationalId = :NationalId';
+
+  { TSimplePerson }
+
+procedure TSimplePerson.Clear;
+begin
+  fPersonId := 0;
+  fDOB := 0;
+  fSex := sexUnknown;
+  fLstName := EmptyStr;
+  fFstName := EmptyStr;
+  fNationalId := EmptyStr;
+end;
+
+function TSimplePerson.Get_Age: double;
+begin
+  Result := ( Now - fDOB ) / 365.25;
+end;
+
+function TSimplePerson.Get_DOB: TDate;
+begin
+  Result := fDOB;
+end;
+
+function TSimplePerson.Get_FirstName: string;
+begin
+  Result := fFstName;
+end;
+
+function TSimplePerson.Get_FullName: string;
+begin
+  Result := Format( '%s, %s', [fLstName, fFstName] );
+end;
+
+function TSimplePerson.Get_GenderId: integer;
+begin
+  Result := ord( fSex );
+end;
+
+function TSimplePerson.Get_LastName: string;
+begin
+  Result := fLstName;
+end;
+
+function TSimplePerson.Get_NationalId: string;
+begin
+  Result := fNationalId;
+end;
+
+function TSimplePerson.Get_PersonId: integer;
+begin
+  Result := fPersonId;
+end;
+
+function TSimplePerson.Get_Sex: TSex;
+begin
+  Result := fSex;
+end;
+
+function TSimplePerson.Get_VisualId: string;
+begin
+  if NationalId = EmptyStr then
+    Result := Format( '%s - %s %s', [DateToStr( DOB ), fFstName, fLstName] )
+  else
+    Result := Format( '%s - %s %s', [NationalId, fFstName, fLstName] );
+end;
+
+procedure TSimplePerson.Set_GenderId( const Value: integer );
+begin
+  case Value of
+    1: fSex := sexMale;
+    2: fSex := sexFemale;
+  else fSex := sexUnknown;
+  end;
+end;
+
+procedure TSimplePerson.Set_PersonId( const Value: integer );
+begin
+  fPersonId := Value;
+end;
+
+function TSimplePerson.ShortId: string;
+begin
+  //
+end;
+
+function TSimplePerson.Valid: boolean;
+begin
+  Result := ( fPersonId > 0 ) and ( fDOB > 0 ) and ( fLstName <> EmptyStr ) and ( fLstName <> EmptyStr );
+end;
+
+procedure TSimplePerson.MapId( const ASQL: ISQL );
+begin
+  { Find person in database }
+  with ASQL.FastQuery( QRY_PERSON, [NationalId] ) do
+    try
+      fPersonId := Fields[0].AsInteger;
+      fDOB := Fields[1].AsDateTime;
+      GenderId := Fields[2].AsInteger;
+      fFstName := Fields[3].AsString;
+      fLstName := Fields[4].AsString;
+      if not( PersonId > 0 ) then
+        raise EPersonNotFoundException.CreateFmt( StrPersonNotFound, [NationalId] );
+    finally
+      Close;
+    end;
+end;
+
+end.
